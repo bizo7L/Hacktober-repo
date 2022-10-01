c7447f4a7e61933baa9e90e9d8107c61d6db1324
bizo7L <114811301+bizo7L@users.noreply.github.com>
2022-10-01 13:28:08 +0100

    fix giga.js

--- giga.js
+++ giga.js
@@ -1,15 +1,17 @@
-var GigaFyde = args;
+const util = require('./util')
+
+var GigaFyde = util.getFirstName(args);
 var rep = 0;
 
 if (!GigaFyde) return;
 
-function (amazing) {
+function amazing(name) {
 
-	if (GigaFyde === "amazing") {
+	if (name === "amazing") {
 		return rep +1;
 	} else {
 		return rep -1;
 	}
 }
 
-amazing();
+amazing(GigaFyde);
